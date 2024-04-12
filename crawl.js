de884922cf70a9ac8f20a85104399b7c92162d4b
Dominic German <dominicgerman@gmail.com>
2024-04-12 08:09:49 -0700

    Added getURLsFromHTML helper function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,23 @@
+const { JSDOM } = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const dom = new JSDOM(htmlBody);
+  const urlObjects = Array.from(dom.window.document.querySelectorAll('a'));
+  const arrayOfURLs = urlObjects.map((el) => {
+    try {
+      if (el.href.startsWith('/')) {
+        const url = new URL(`${baseURL}${el.href}`);
+        return url.href;
+      }
+      const url = new URL(el.href);
+      return url.href;
+    } catch (err) {
+      console.log('Error from getURLsFromHTML:', err.message);
+    }
+  });
+  return arrayOfURLs;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const newURLString = urlObj.hostname + urlObj.pathname;
@@ -9,4 +29,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@jest/globals');
-const { normalizeURL } = require('./crawl.js');
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js');
 
 test('normalizeURL strip protocol', () => {
   const input = 'https://dominicgerman.com/about';
@@ -19,5 +19,65 @@ test('normalizeURL capitals', () => {
   const input = 'https://DOMINICGERMAN.COM/about/';
   const acutal = normalizeURL(input);
   const expected = 'dominicgerman.com/about';
+  expect(acutal).toEqual(expected);
+});
+
+test('getURLsFromHTML absolute', () => {
+  const inputHTML = `
+  <html>
+    <body>
+        <a href="https://dominicgerman.com/about"><span>Go to my website</span></a>
+    </body>
+  </html>
+  `;
+  const inputURL = 'https://dominicgerman.com/about';
+  const acutal = getURLsFromHTML(inputHTML, inputURL);
+  const expected = ['https://dominicgerman.com/about'];
+  expect(acutal).toEqual(expected);
+});
+
+test('getURLsFromHTML relative', () => {
+  const inputHTML = `
+  <html>
+    <body>
+        <a href="/about"><span>Go to my website</span></a>
+    </body>
+  </html>
+  `;
+  const inputURL = 'https://dominicgerman.com';
+  const acutal = getURLsFromHTML(inputHTML, inputURL);
+  const expected = ['https://dominicgerman.com/about'];
+  expect(acutal).toEqual(expected);
+});
+
+test('getURLsFromHTML both relative and absolute', () => {
+  const inputHTML = `
+  <html>
+    <body>
+        <a href="/resume"><span>Go to my resume</span></a>
+        <a href="https://dominicgerman.com/about"><span>Go to my website</span></a>
+    </body>
+  </html>
+  `;
+  const inputURL = 'https://dominicgerman.com';
+  const acutal = getURLsFromHTML(inputHTML, inputURL);
+  const expected = [
+    'https://dominicgerman.com/resume',
+    'https://dominicgerman.com/about',
+  ];
+  expect(acutal).toEqual(expected);
+});
+
+test('getURLsFromHTML invalid', () => {
+  const inputHTML = `
+  <html>
+    <body>
+        <a href="invalid"><span>An invalid URL</span></a>
+    </body>
+  </html>
+  `;
+  const inputURL = 'https://dominicgerman.com';
+  const acutal = getURLsFromHTML(inputHTML, inputURL);
+  const expected = [];
   expect(acutal).toEqual(expected);
 });
